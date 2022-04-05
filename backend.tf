9e3ce6eff75358f31e80ae9f5d33cc33085d04eb
Ilja Verbovetski <96315345+iljailz@users.noreply.github.com>
2022-04-05 11:22:45 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "my-bucket-jenkins"
+        bucket = "my-bucket-jenkinsclass"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
